0100642c9d463df11410bd171504b4b8de948297
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-10-14 18:13:01 +0300

    Adding 'made by' in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,23 +4,28 @@ import FooterSocial from "./footer-social";
 
 export default function Footer() {
     return (
-        <footer className="bg-danger">
-            <div className="container-fluid container-lg">
-                <div className="d-flex flex-row justify-content-center py-5">
-                    <div className="d-flex flex-row col-6 justify-content-center align-items-center">
-                        <p className="text-light col-8">
-                            <b>Gidmi</b> is a website where you can follow a guide for Next fullstack application.
-                        </p>
-                    </div>
-                    <div className="d-flex flex-row col-6 justify-content-evenly">
-                        <div className="d-flex flex-column">
-                            <FooterLinks/>
+        <footer>
+            <div className="bg-danger">
+                <div className="container-fluid container-lg">
+                    <div className="d-flex flex-row justify-content-center py-5">
+                        <div className="d-flex flex-row col-6 justify-content-center align-items-center">
+                            <p className="text-light col-8">
+                                <b>Gidmi</b> is a website where you can follow a guide for Next fullstack application.
+                            </p>
                         </div>
-                        <div className="d-flex flex-column">
-                            <FooterSocial/>
+                        <div className="d-flex flex-row col-6 justify-content-evenly">
+                            <div className="d-flex flex-column">
+                                <FooterLinks/>
+                            </div>
+                            <div className="d-flex flex-column">
+                                <FooterSocial/>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="d-flex flex-row justify-content-center align-items-center footer-made-by">
+                <span className="py-2 text-light">Made by Gimmy Razafimbelo</span>
             </div>
         </footer>
     );
