ea59000452cdf420eb2492677c9e2feeb314f550
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-10-15 18:40:21 +0300

    Editing footer

--- src/components/footer-social.tsx
+++ src/components/footer-social.tsx
@@ -6,7 +6,7 @@ export default function FooterSocial() {
             <strong className="text-light mb-1">Social</strong>
             <ul className="nav flex-column">
                 <Link href="https://www.linkedin.com/in/gimmy-razafimbelo-7a0154168/" className="nav-link text-gray px-0 py-0">LinkedIn</Link>
-                <Link href="https://github.com/GimmyR" className="nav-link text-gray px-0 py-0">GitHub</Link>
+                <Link href="https://github.com/GimmyR/gidmi" className="nav-link text-gray px-0 py-0">GitHub</Link>
             </ul>
         </>
     );
