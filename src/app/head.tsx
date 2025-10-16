e1c421e89665ab4e6a1aa80643f677be4ce0c2f8
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-10-16 16:58:28 +0300

    Adding script to help Google Adsense for verification (4)

--- src/app/head.tsx
+++
@@ -1,9 +0,0 @@
-// app/head.tsx
-export default function Head() {
-    return (
-        <>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9490135232409415" crossOrigin="anonymous"></script>
-        </>
-    );
-  }
-  
