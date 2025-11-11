1854bc95e5911830e6771ea38972cc72f81cf44d
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-11-11 18:19:55 +0300

    Adding dynamic metadata for parts

--- src/app/part/[id]/page.tsx
+++ src/app/part/[id]/page.tsx
@@ -2,7 +2,19 @@ import findAllPartsByGuideId from "@/actions/find-all-parts-by-guide-id";
 import findPartById from "@/actions/find-part-by-id";
 import Guide from "@/components/guide";
 
-export default async function PartPage({ params } : { params: Promise<{ id: string }> }) {
+type Props = { 
+    params: Promise<{ id: string }>
+};
+
+export async function generateMetadata({ params } : Props) {
+    const { id } = await params;
+    const part = await findPartById(id);
+    return {
+        title: part?.title + " - Gidmi"
+    };
+}
+
+export default async function PartPage({ params } : Props) {
     const { id } = await params;
     const part = await findPartById(id);
     const parts = await findAllPartsByGuideId(part?.guideId);
