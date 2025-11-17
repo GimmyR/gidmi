74e428336e8d2acfa3d98715d22b098ab536f9d8
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-11-17 11:59:34 +0300

    Editing adsense style on navigation menu

--- src/components/guide.jsx
+++ src/components/guide.jsx
@@ -129,7 +129,7 @@ export default async function Guide({ title, parts, details }) {
 				</div>
 				<div className="offcanvas-body d-flex flex-column px-3">
 					<NavMenu parts={parts} selected={title}/>
-					<Adsense format="rectangle"/>
+					<Adsense format="vertical"/>
 				</div>
 			</div>
 			<div className="col-12 col-lg-6 p-2 p-lg-0">
