077197f38d9c8acb5b828e127b5a105b66e04f3c
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-11-17 11:05:12 +0300

    Adding ad blocks manually

---
+++ src/components/adsense.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Adsense({ format = "auto" } : { format: string }) {
+    useEffect(() => {
+        try {
+            //@ts-ignore
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        } catch(e) {
+            console.log(e)
+        }
+    });
+
+    return (
+        <ins className="adsbygoogle"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-9490135232409415"
+            data-ad-slot="5964084213"
+            data-ad-format={format}
+            data-full-width-responsive="true">
+        </ins>
+    );
+}
--- src/components/guide.jsx
+++ src/components/guide.jsx
@@ -1,6 +1,7 @@
 import NavMenu from "@/components/navigation-menu";
 import GuideContent from "./guide-content";
 import PreviousNext from "./previous-next";
+import Adsense from "./adsense";
 
 export default async function Guide({ title, parts, details }) {
 	const findPrevious = () => {
@@ -128,12 +129,17 @@ export default async function Guide({ title, parts, details }) {
 				</div>
 				<div className="offcanvas-body d-flex flex-column px-3">
 					<NavMenu parts={parts} selected={title}/>
+					<Adsense format="square"/>
 				</div>
 			</div>
 			<div className="col-12 col-lg-6 p-2 p-lg-0">
 				<h1 className="mb-5">{title}</h1>
 				{details.map((detail) => <GuideContent key={detail.id} detail={detail}/>)}
+				<Adsense format="horizontal"/>
 				<PreviousNext previous={findPrevious()} next={findNext()}/>
+			</div>
+			<div className="offcanvas-lg col-lg-3">
+				<Adsense format="vertical"/>
 			</div>
 		</div>
     );
