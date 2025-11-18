21ad0d7c47eec3dfb3fe44df26db158fc1e1cac3
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-11-18 10:16:24 +0300

    Switching adsense from block to inline-block

--- src/components/adsense.tsx
+++ src/components/adsense.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 
-export default function Adsense({ format = "auto" } : { align: string, format: string }) {
+export default function Adsense({ align = "text-start", format = "auto" } : { align: string, format: string }) {
     useEffect(() => {
         const timer = setTimeout(() => {
             try {
@@ -12,17 +12,19 @@ export default function Adsense({ format = "auto" } : { align: string, format: s
                 console.log(e)
             }
         }, 200);
-        
+
         return () => clearTimeout(timer);
     });
 
     return (
-        <ins className="adsbygoogle"
-            style={{ display: "block", width: "100%", minHeight: "100px" }}
-            data-ad-client="ca-pub-9490135232409415"
-            data-ad-slot="5964084213"
-            data-ad-format={format}
-            data-full-width-responsive="true">
-        </ins>
+        <div className={align}>
+            <ins className="adsbygoogle"
+                style={{ display: "inline-block", width: "100%", minHeight: "100px" }}
+                data-ad-client="ca-pub-9490135232409415"
+                data-ad-slot="5964084213"
+                data-ad-format={format}
+                data-full-width-responsive="true">
+            </ins>
+        </div>
     );
 }
