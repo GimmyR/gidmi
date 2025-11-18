7df524eddfd61001e9909ba0e3add64fac4ea605
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-11-18 13:27:18 +0300

    Stop using responsive

--- src/components/adsense.tsx
+++ src/components/adsense.tsx
@@ -3,13 +3,13 @@
 import { useEffect, useRef } from "react";
 
 const style = {
-    display: "block", 
+    display: "inline-block", 
     width: "160px", 
     height: "600px"
 };
 
 export default function Adsense({ slot } : { slot: string }) {
-    const ref = useRef<HTMLDivElement>(null);
+    const ref = useRef(null);
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -27,13 +27,10 @@ export default function Adsense({ slot } : { slot: string }) {
     });
 
     return (
-        <div ref={ref} style={{...style, margin: "0 auto"}}>
-            <ins className="adsbygoogle"
-                style={style}
-                data-ad-client="ca-pub-9490135232409415"
-                data-ad-slot={slot}
-                data-ad-format="vertical">
-            </ins>
-        </div>
+        <ins ref={ref} className="adsbygoogle"
+            style={style}
+            data-ad-client="ca-pub-9490135232409415"
+            data-ad-slot={slot}>
+        </ins>
     );
 }
