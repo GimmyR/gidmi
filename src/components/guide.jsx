465487c9fbbb04d1e89eb89eb690655d1d503f63
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-11-18 10:33:04 +0300

    Editing format of ads in the lower of the page

--- src/components/guide.jsx
+++ src/components/guide.jsx
@@ -135,7 +135,7 @@ export default async function Guide({ title, parts, details }) {
 			<div className="col-12 col-lg-6 p-2 p-lg-0">
 				<h1 className="mb-5">{title}</h1>
 				{details.map((detail) => <GuideContent key={detail.id} detail={detail}/>)}
-				<Adsense format="horizontal"/>
+				<Adsense format="auto"/>
 				<PreviousNext previous={findPrevious()} next={findNext()}/>
 			</div>
 			<div className="d-none d-lg-block col-lg-3">
