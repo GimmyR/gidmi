3f15fe9ba8589f46485c169e531ffa88d97be412
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-11-18 12:58:16 +0300

    Using ref and increasing delay of setTimeout

--- src/components/adsense.tsx
+++ src/components/adsense.tsx
@@ -1,23 +1,26 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function Adsense({ slot, className } : { slot: string, className: string }) {
+    const ref = useRef(null);
     useEffect(() => {
         const timer = setTimeout(() => {
-            try {
-                //@ts-ignore
-                (window.adsbygoogle = window.adsbygoogle || []).push({});
-            } catch(e) {
-                console.log(e)
+            if(ref.current) {
+                try {
+                    //@ts-ignore
+                    (window.adsbygoogle = window.adsbygoogle || []).push({});
+                } catch(e) {
+                    console.log(e)
+                }
             }
-        }, 200);
+        }, 500);
         
         return () => clearTimeout(timer);
     });
 
     return (
-        <ins className={`adsbygoogle ${className}`}
+        <ins ref={ref} className={`adsbygoogle ${className}`}
             style={{ display: "block", width: "100%", minHeight: "auto", height: "auto" }}
             data-ad-client="ca-pub-9490135232409415"
             data-ad-slot={slot}
