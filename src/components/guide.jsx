410685c5010af5f5de05bf191eede80243a63e95
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-11-19 10:45:03 +0300

    Editing style of adsense

--- src/components/guide.jsx
+++ src/components/guide.jsx
@@ -130,7 +130,7 @@ export default async function Guide({ title, parts, details }) {
 				</div>
 				<div className="offcanvas-body d-flex flex-column align-items-center px-3">
 					<NavMenu parts={parts} selected={title}/>
-					<VerticalAdsense className="my-5"/>
+					<VerticalAdsense className="mt-4 mb-5"/>
 				</div>
 			</div>
 			<div className="col-12 col-lg-6 p-2 p-lg-0">
