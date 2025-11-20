d0b37227b5f0e94dcd974b4c3fa4c9d3d52be199
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-11-20 15:45:32 +0300

    Removing ref

--- src/components/article-adsense.tsx
+++ src/components/article-adsense.tsx
@@ -3,17 +3,13 @@
 import { useEffect, useRef } from "react";
 
 export default function ArticleAdsense({ className = "" } : { className: string }) {
-    const ref = useRef(null);
-
     useEffect(() => {
         const timer = setTimeout(() => {
-            if(ref.current) {
-                try {
-                    //@ts-ignore
-                    (window.adsbygoogle = window.adsbygoogle || []).push({});
-                } catch(e) {
-                    console.log(e)
-                }
+            try {
+                //@ts-ignore
+                (window.adsbygoogle = window.adsbygoogle || []).push({});
+            } catch(e) {
+                console.log(e)
             }
         }, 200);
         
@@ -21,7 +17,7 @@ export default function ArticleAdsense({ className = "" } : { className: string 
     });
 
     return (
-        <ins ref={ref} className={ `adsbygoogle d-block text-center ${className}` }
+        <ins className={ `adsbygoogle d-block text-center ${className}` }
             data-ad-layout="in-article"
             data-ad-format="fluid"
             data-ad-client="ca-pub-9490135232409415"
--- src/components/vertical-adsense.tsx
+++ src/components/vertical-adsense.tsx
@@ -3,17 +3,13 @@
 import { useEffect, useRef } from "react";
 
 export default function VerticalAdsense({ className = "" } : { className: string }) {
-    const ref = useRef(null);
-
     useEffect(() => {
         const timer = setTimeout(() => {
-            if(ref.current) {
-                try {
-                    //@ts-ignore
-                    (window.adsbygoogle = window.adsbygoogle || []).push({});
-                } catch(e) {
-                    console.log(e)
-                }
+            try {
+                //@ts-ignore
+                (window.adsbygoogle = window.adsbygoogle || []).push({});
+            } catch(e) {
+                console.log(e)
             }
         }, 200);
         
@@ -21,7 +17,7 @@ export default function VerticalAdsense({ className = "" } : { className: string
     });
 
     return (
-        <ins ref={ref} className={ `adsbygoogle d-block text-center ${className}` }
+        <ins className={ `adsbygoogle d-block text-center ${className}` }
             data-ad-client="ca-pub-9490135232409415"
             data-ad-slot="5964084213"
             data-ad-format="auto"
