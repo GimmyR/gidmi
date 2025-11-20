d28b8258a42d11666719b271f005755561b28174
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-11-20 17:23:05 +0300

    Hiding ads box if ad is unfilled

--- src/components/article-adsense.tsx
+++ src/components/article-adsense.tsx
@@ -17,8 +17,8 @@ export default function ArticleAdsense({ className = "" } : { className: string 
     });
 
     return (
-        <div className={ `d-block col-12 ${className}` }>
-            <ins className="adsbygoogle d-block text-center"
+        <div className="d-block col-12">
+            <ins className={`adsbygoogle ${className} article-ads`}
                 data-ad-layout="in-article"
                 data-ad-format="fluid"
                 data-ad-client="ca-pub-9490135232409415"
