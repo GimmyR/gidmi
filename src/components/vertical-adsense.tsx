8aecb536236a38707ae7c9230d2174a36f98dd9a
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-11-20 16:32:46 +0300

    Enabling data full width responsive

--- src/components/vertical-adsense.tsx
+++ src/components/vertical-adsense.tsx
@@ -22,7 +22,7 @@ export default function VerticalAdsense({ className = "" } : { className: string
                 data-ad-client="ca-pub-9490135232409415"
                 data-ad-slot="5964084213"
                 data-ad-format="vertical"
-                data-full-width-responsive="false">
+                data-full-width-responsive="true">
             </ins>
         </div>
     );
