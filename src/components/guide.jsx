11cebb9eb24ed489c962da9ac3237406110cc377
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-11-21 20:37:31 +0300

    Removing all ad units (I want to use auto ads)

--- src/components/guide.jsx
+++ src/components/guide.jsx
@@ -1,8 +1,6 @@
 import NavMenu from "@/components/navigation-menu";
-import GuideContent from "./guide-content";
 import PreviousNext from "./previous-next";
-import VerticalAdsense from "./vertical-adsense";
-import ArticleAdsense from "./article-adsense";
+import GuideContentByType from "./guide-content-by-type";
 
 export default async function Guide({ title, parts, details }) {
 	const findPrevious = () => {
@@ -123,25 +121,23 @@ export default async function Guide({ title, parts, details }) {
 
     return (
 		<div className="container-fluid d-flex flex-row min-vh-100 mt-5 pt-5 px-0 text-light">
-			<div className="offcanvas-lg offcanvas-start col-4 col-lg-3 bg-dark text-light" id="navigation" tabIndex={-1} aria-labelledby="navigationLabel">
+			<nav className="offcanvas-lg offcanvas-start col-4 col-lg-3 bg-dark text-light" id="navigation" tabIndex={-1} aria-labelledby="navigationLabel">
 				<div className="offcanvas-header">
 					<h5 className="offcanvas-title" id="navigationLabel">Menu</h5>
 					<button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" data-bs-target="#navigation" aria-label="Close"></button>
 				</div>
 				<div className="offcanvas-body d-flex flex-column align-items-center px-3">
 					<NavMenu parts={parts} selected={title}/>
-					<VerticalAdsense className="mt-4 mb-5"/>
 				</div>
-			</div>
-			<div className="d-flex flex-column col-12 col-lg-6 p-2 p-lg-0">
-				<h1 className="mb-5">{title}</h1>
-				{details.map((detail) => <GuideContent key={detail.id} detail={detail}/>)}
-				<ArticleAdsense className="my-5"/>
+			</nav>
+			<main className="d-flex flex-column min-vh-100 col-12 col-lg-6 p-2 p-lg-0">
+				<section>
+					<h1 className="mb-5">{title}</h1>
+					{details.map((detail) => <GuideContentByType key={detail.id} detail={detail}/>)}
+				</section>
 				<PreviousNext previous={findPrevious()} next={findNext()}/>
-			</div>
-			<div className="d-none d-lg-block col-lg-3 px-3">
-				<VerticalAdsense className="mt-2"/>
-			</div>
+			</main>
+			<aside className="d-none d-lg-flex justify-content-center col-lg-3 px-3"></aside>
 		</div>
     );
 }
--- src/components/previous-next.tsx
+++ src/components/previous-next.tsx
@@ -8,13 +8,13 @@ type Endpoint = {
 
 export default function PreviousNext({ previous, next } : { previous: Endpoint | undefined, next: Endpoint | undefined }) {
     return (
-        <div className="d-flex flex-row my-5">
+        <section className="d-flex flex-row my-5">
             <div className="d-flex flex-row justify-content-start col-6">
                 <PreviousLink link={previous}/>
             </div>
             <div className="d-flex flex-row justify-content-end col-6">
                 <NextLink link={next}/>
             </div>
-        </div>
+        </section>
     );
 }
