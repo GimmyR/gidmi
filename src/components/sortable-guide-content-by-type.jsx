3e4d6dbcf4e1badae7258f16bd428b6d77e9a37a
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2025-12-20 08:35:37 +0300

    Creating Sortable GuideContentByType

---
+++ src/components/sortable-guide-content-by-type.jsx
@@ -0,0 +1,18 @@
+import CommandSnippet from "./command-snippet";
+import CodeSnippet from "./code-snippet";
+
+export default function SortableGuideContentByType({ detail }) {
+    return (
+        <>
+            {detail.type === "text" ?
+                <div dangerouslySetInnerHTML={{ __html: detail.content }}></div>
+            : detail.type === "command" ?
+                <CommandSnippet>{detail.content}</CommandSnippet>
+            : detail.type === "image" ?
+                <div className="d-flex flex-row justify-content-center mb-5">
+                    <img src={`/images/${detail.content}`} className='img-fluid'/>
+                </div>
+            : <CodeSnippet language={detail.type} code={detail.content}/>}
+        </>
+    );
+}
